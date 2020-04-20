f0db72059ff873c035d8428a9976b2c4c40909c6
ilavisharma <lavisharma796@gmail.com>
2020-04-20 23:19:03 +0530

    center the content

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -3,13 +3,13 @@ import Layout from '../components/Layout';
 
 const Home = () => (
   <Layout>
-    <h2 className="display-2">Knex-Java</h2>
-    <h5 className="well">
+    <h2 className="display-2 text-center">Knex-Java</h2>
+    <h5 className="well text-center">
       A SQL query builder that is flexible, portable, and fun to use!
     </h5>
     <hr />
     Get up and running with a database connection in just 2 steps
-    <img src="/setup.png" />
+    <img src="/setup.png" alt="setup instructions" className="img-fluid rounded mx-auto d-block" />
   </Layout>
 );
 
